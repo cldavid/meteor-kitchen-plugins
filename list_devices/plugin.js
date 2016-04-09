5c34bf207fe1443cc13953bf571764d7a1aa30d0
David Cluytens <david.cluytens@softathome.com>
2016-04-09 20:53:16 +0200

    dynamic dropdown list

--- list_devices/plugin.js
+++ list_devices/plugin.js
@@ -9,24 +9,61 @@ component.html += '     {{#each device_list}}';
 component.html += '     <option value=\"{{this}}\">{{this}}</option>';
 component.html += '     {{/each}}';
 component.html += '</select>';
+component.html += '<select id=\"sensor_list-select\">';
+component.html += ' <option disabled=\"disabled\" selected=\"selected\">Please Select</option>';
+component.html += '     {{#each sensor_list}}';
+component.html += '     <option value=\"{{this}}\">{{this}}</option>';
+component.html += '     {{/each}}';
+component.html += '</select>';
 component.html += '</template>';
 component.js = function () {
 
+    Template.TEMPLATE_NAME.onCreated(function() {
+        this.templateDictionary = new ReactiveDict();
+        this.templateDictionary.set('updateSensorList', undefined);
+    });
+
     Template.TEMPLATE_NAME.helpers({
         device_list: function(){
-            var data    = Devices.find({device: "arduino01"}).fetch();
+            var data    = Devices.find({}).fetch();
             var devices = _.pluck(data, 'device');
             return _.uniq(devices);
+        },
+        sensor_list: function(){
+            return;
         }
     });
 
     Template.TEMPLATE_NAME.events({
             "change #device_list-select": function (event, template) {
+                var device = $(event.currentTarget).val();
+                var data    = Devices.find({device: device}).fetch();
+                var sensors = _.pluck(data, 'sensor');
+
+                var sensorsList = document.getElementById('sensor_list-select');
+                if (sensorsList == null) {
+                  alert('list box is null');
+                  return;
+                }
+                sensorsList.options.length = 0;
+                var opt = document.createElement('option');
+                opt.innerHTML = "Please Select";
+                opt.disabled = true;
+                opt.selected = true;
+                sensorsList.appendChild(opt);
+                _.each(sensors, function(sensor) {
+                  var opt = document.createElement('option');
+                  opt.value = sensor;
+                  opt.innerHTML = sensor;
+                  sensorsList.appendChild(opt);
+                });
+            },
+            "change #sensor_list-select": function (event, template) {
                 var category = $(event.currentTarget).val();
-                console.log("log: device_list : " + category);
+                console.log("sensor_list : " + category);
             }
+
     });
 };
 
 kitchen.setOutput(component);
-
